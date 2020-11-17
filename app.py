11d1c82b8e39adb64e122674fdccb8aa06b57c11
Saurabh <saurabhmehta1601@gmail.com>
2020-11-17 16:46:37 +0530

    grayscale image can be downloaded

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import (Flask,redirect,render_template,request)
+from flask import (Flask,redirect,render_template,request,send_file)
 from werkzeug.utils import secure_filename
+from processor import processor
 import os
 
 # Creating flask app instance
@@ -7,7 +8,7 @@ app=Flask(__name__)
 
 # Setting Upload folder and extentions for file 
 ALLOWED_EXTENTIONS={'png','jpg','jpeg'}
-UPLOAD_FOLDER='./images'
+UPLOAD_FOLDER='./images/'
 #################
 app.config['ALLOWED_EXTENTIONS']=ALLOWED_EXTENTIONS
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
@@ -21,12 +22,23 @@ def home():
 # Route for handling file upload
 @app.route('/process',methods=["POST"])
 def process():
+    # uploaded file store in varible f
     f=request.files['file']
+    # if f have appropriate extention save uploaded file in image directory
     if f.filename.split('.')[1] in app.config["ALLOWED_EXTENTIONS"]:
         f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-        return redirect(request.referrer)
+        # After saving uploaded image process the image applying processor method on it
+        processor(f.filename,app.config['UPLOAD_FOLDER'])
+        # redirect to route which downloads the final grayscale image
+        return redirect('/download/'+f.filename)
     else:
         return "Invalid file extention"
+# route to download image
+@app.route('/download/<filename>')
+def download(filename):
+    filename=app.config['UPLOAD_FOLDER']+filename
+    # downloads image
+    return send_file(filename,as_attachment=True)
 
 
 if __name__ == "__main__":
--- processor.py
+++ processor.py
@@ -1,10 +1,9 @@
 from PIL import Image
 
 
-def processor(filename):
-    file='./images/'+filename
+def processor(filename,filepath):
+    file=filepath+filename
     img=Image.open(file)
     greyimg=img.convert('L')
     greyimg.save(file)
 
-processor('5XqgSe.jpg')
